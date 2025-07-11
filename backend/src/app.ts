359730c954d3a11424d175911ee1118a615e718e
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-07-11 23:51:57 +0600

    implement cors and deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,11 @@ import borrowRouter from "./app/routes/borrowRoutes";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://library-seven-psi.vercel.app", "http://localhost:5173"],
+  })
+);
 app.use("/api/books", bookRouter);
 app.use("/api/borrow", borrowRouter);
 
